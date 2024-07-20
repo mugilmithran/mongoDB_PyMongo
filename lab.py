e2ccdcc6749838345e9f00648a438152c7521c1d
Mugilmithran <80945146+mugilmithran@users.noreply.github.com>
2024-07-19 22:00:57 -0400

    Update lab.py

--- lab.py
+++ lab.py
@@ -13,35 +13,35 @@ conn = f'mongodb+srv://mugilmithran01:{password}@cluster0.cfxmvyc.mongodb.net/?r
 client = pymongo.MongoClient(conn)
 
 # Creating a database named 'students'.
-# students = client.students
+students = client.students
 
 # Creating a collection named 'details'.
-# details = students.details
+details = students.details
 
 # Inserting one document to the collection.
-# details.insert_one({"Name": "Mugilmithran", "Age": 20, "Course": "Bigdata"})
+details.insert_one({"Name": "Mugilmithran", "Age": 20, "Course": "Bigdata"})
 
 # # Inserting multiple documents to the collection.
-# details.insert_many([{"Name": "Ragul", "Age": 26, "Course": "Bigdata"}, 
-#                      {"Name": "Rajeev", "Age": 24, "Course": "Bigdata"}, 
-#                      {"Name": "Ramnath", "Age": 22, "Course": "Bigdata"},
-#                      {"Name": "Bo", "Age": 30, "Course": "Bigdata"}])
+details.insert_many([{"Name": "Ragul", "Age": 26, "Course": "Bigdata"}, 
+                     {"Name": "Rajeev", "Age": 24, "Course": "Bigdata"}, 
+                     {"Name": "Ramnath", "Age": 22, "Course": "Bigdata"},
+                     {"Name": "Bo", "Age": 30, "Course": "Bigdata"}])
 
 # Updating a document's 'Name' by '_id' from the collection.
-# details.update_one({"Name": "Mugilmithran"}, {"$set": {"Name": "Kathiravan"}})
+details.update_one({"Name": "Mugilmithran"}, {"$set": {"Name": "Kathiravan"}})
 
 # Deleting a document, which is named as 'Mugilmithran'
-# details.delete_one({'Name': 'Mugilmithran'})
+details.delete_one({'Name': 'Mugilmithran'})
 
 #Initializing a document relationship.
-# address = {
-#     "Street": "Bay street",
-#     "Number": 2706,
-#     "City": "San francisco",
-#     "Country": "Country"
-# }
+address = {
+    "Street": "Bay street",
+    "Number": 2706,
+    "City": "San francisco",
+    "Country": "Country"
+}
 
-# details.update_one({"Name": "Ragul"}, {"$addToSet": {"address": address}})
+details.update_one({"Name": "Ragul"}, {"$addToSet": {"address": address}})
 
 
 
